bfc0ca06d2fd303f28f5ad3af0f977b3e03db19f
Louis-Auguste Dumas <louis-auguste.dumas@epitech.eu>
2018-12-19 13:26:10 +0100

    bug patch

--- get_next_line.c
+++ get_next_line.c
@@ -13,7 +13,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#define SIZE 1
+#define READ_SIZE 1
 
 unsigned int my_len(char const *str)
 {
@@ -25,55 +25,58 @@ unsigned int my_len(char const *str)
     return (i);
 }
 
-bool find_char(char const *str, char const c)
+bool find_char(char const *str, char const c, char const d)
 {
     for (unsigned int i = 0; str && str[i]; i++) {
         if (c == str[i])
+            return (true);
+        if (d == str[i])
             return (true);
     }
     return (false);
 }
 
-char *my_starcat_gnl(char *s1, char *s2, unsigned int *i, int fd)
+char *my_starcat_gnl(char *tmp_ret, char *buf, unsigned int *i, int fd)
 {
-    char *tmp = malloc(sizeof(char) * (my_len(s1) + my_len(s2 + *i) + 1));
-    unsigned int copy = 0;
+    char *tmp = malloc(sizeof(char) * (my_len(tmp_ret) + my_len(buf + *i) + 1));
+    unsigned int cpy = 0;
 
     if (tmp != NULL)
-        tmp[my_len(s1) + my_len(s2 + *i)] = tmp[0] = '\0';
-    for (int j = 0; s1 && tmp && s1[j]; j++, copy++)
-        tmp[copy] = s1[j];
-    for (; s2 && tmp && s2[*i] != '\0' && s2[*i] != '\n'; *i += 1, copy++)
-        tmp[copy] = s2[*i];
-    tmp[copy] = '\0';
-    *i = (s2[*i] == '\0') ? 0 : *i + 1;
-    for (unsigned int k = 0; s2 && s1 && k <= SIZE; k++) {
-        (fd == 0) ? (s2[k] = '\0'), (s1[k] = '\0') : (s1[k] = '\0');
+        tmp[my_len(tmp_ret) + my_len(buf + *i)] = tmp[0] = '\0';
+    for (; tmp_ret && tmp && tmp_ret[cpy]; cpy++)
+        tmp[cpy] = tmp_ret[cpy];
+    for (; tmp && buf[*i] && buf[*i] != '\n'; *i += 1, cpy++)
+        tmp[cpy] = buf[*i];
+    (tmp != NULL) ? (tmp[cpy] = '\0') : 0 ;
+    *i = (buf[*i] == '\0' || buf[*i] == EOF) ? 0 : *i + 1;
+    for (unsigned int k = 0; buf && tmp_ret && k <= READ_SIZE; k++) {
+        (fd == 0) ? (buf[k] = '\0'), (tmp_ret[k] = '\0') : (tmp_ret[k] = '\0');
     }
-    free(s1);
+    free(tmp_ret);
     return (tmp);
 }
 
 char *get_next_line(int fd)
 {
-    static char buffer[SIZE + 1];
-    char *tmp = malloc(sizeof(char) * SIZE + 1);
+    static char buf[READ_SIZE + 1];
+    char *tmp = malloc(sizeof(char) * (READ_SIZE + 1));
     static unsigned int i = 0;
 
-    if (tmp != NULL)
-        buffer[SIZE] = tmp[SIZE] = tmp[0] = buffer[0] = '\0';
-    if (tmp && i != 0) {
-        if (find_char(buffer + i, '\n') == true)
-            return (my_starcat_gnl(tmp, buffer, &i, fd));
-        tmp = my_starcat_gnl(tmp, buffer, &i, fd);
+    if (tmp == NULL)
+        return (NULL);
+    buf[READ_SIZE] = tmp[READ_SIZE] = tmp[0] = buf[0] = '\0';
+    if (i != 0) {
+        if (find_char(buf + i, '\n', EOF) == true)
+            return (my_starcat_gnl(tmp, buf, &i, fd));
+        tmp = my_starcat_gnl(tmp, buf, &i, fd);
     }
-    for (int len = 0; tmp && (len = read(fd, buffer, SIZE)) != 0; ) {
+    for (int len = 0; (len = read(fd, buf, READ_SIZE)) > 0; ) {
         if (len == -1)
             return (NULL);
-        else if (find_char(buffer, '\n') == true)
-            return (my_starcat_gnl(tmp, buffer, &i, fd));
+        else if (find_char(buf, '\n', '\0') == true)
+            return (my_starcat_gnl(tmp, buf, &i, fd));
         else
-            tmp = my_starcat_gnl(tmp, buffer, &i, fd);
+            tmp = my_starcat_gnl(tmp, buf, &i, fd);
     }
     return (NULL);
 }
